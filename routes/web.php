949874fa8c1c8546f259b91a6d13c2ac698d04b6
Ravi Singh <ravisingh9990@gmail.com>
2025-12-09 23:51:03 +0530

    Converted html template into laravel

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,21 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Frontend\HomeController;
+use App\Http\Controllers\Frontend\BlogController;
+use App\Http\Controllers\Frontend\ContactController;
+use App\Http\Controllers\Frontend\PortfolioController;
+use App\Http\Controllers\Frontend\ProjectController;
+use App\Http\Controllers\Frontend\PublicationController;
+use App\Http\Controllers\Frontend\ServiceController;
+use App\Http\Controllers\Frontend\TeamController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/blog', [BlogController::class, 'index']);
+Route::get('/project', [ProjectController::class, 'index']);
+Route::get('/portfolio', [PortfolioController::class, 'index']);
+Route::get('/service', [ServiceController::class, 'index']);
+Route::get('/publication', [PublicationController::class, 'index']);
+Route::get('/team', [TeamController::class, 'index']);
+Route::get('/contact', [ContactController::class, 'index']);
+Route::get('/404', [NotFoundController::class, 'index']);
